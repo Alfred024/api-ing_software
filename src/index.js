d42ee16d499b582f3f8f20733285832840eb66af
Alfredo <21030761@itcelaya.edu.mx>
2024-05-02 12:07:48 -0600

    First commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import usersRoutes from "./routes/users.routes.js";
+import morgan from "morgan";
+import { PORT } from "./config.js";
+
+const app = express();
+
+app.use(morgan("dev"));
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(usersRoutes);
+app.get('/', (req, res)=>{
+    res.send('HOLA DESDE LA API');
+});
+app.listen(PORT);
+// eslint-disable-next-line no-console
+console.log("Server on port", PORT);
